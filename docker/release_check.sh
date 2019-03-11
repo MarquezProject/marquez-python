deb48299ca3c81e7c19c8a607b290be73f4e083c
Aleks Shulman <ashulman@NYC-US07347-MBP.local>
2019-03-11 12:45:03 -0700

    Testing changes

--- docker/release_check.sh
+++ docker/release_check.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-function does_package_version_match()
+function check_package_version_match()
 { 
-  echo "running from $(pwd)"
-  which git
-  ls -la
-  ls -ltr
-  git remote show
-  expected_version=$(git describe --tags | head -n 1)
+  expected_version=${CIRCLE_TAG}
   current_version=$(python ./setup.py --version)
   echo "current_version is ${current_version}"
   echo "expected_version is ${expected_version}"
 
   if [[ "${current_version}" != "${expected_version}" ]]; then
 
-    echo "no match"
+    echo "ERROR: mismatch between package version ${current_version} and tag ${expected_version}"
     return 1
   else
-
     echo "match"
     return 0
   fi
 }
 
-does_package_version_match
+check_package_version_match
